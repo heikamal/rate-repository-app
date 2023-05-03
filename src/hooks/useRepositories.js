a34e3cac3b7b607d75e1ba12fabce38038688517
Heikki Malkavaara <heimal@student.uef.fi>
2023-05-03 13:38:55 +0300

    tehtävä 10.11 tehty

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,15 @@
+import { useQuery } from "@apollo/client";
+import { GET_REPOSITORIES } from "../graphql/queries";
+
+const useRepositories = () => {
+	const response = useQuery(GET_REPOSITORIES, {
+		fetchPolicy: 'cache-and-network',
+	})
+
+	const repositories = response.loading ? undefined : response.data.repositories
+	const loading = response.loading;
+
+  return { repositories, loading };
+}
+
+export default useRepositories;
